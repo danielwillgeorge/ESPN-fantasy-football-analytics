7e248b6d8802d2b6fc7fddbcca1fa2da658a46a8
Daniel George <daniel.will.george@gmail.com>
2016-03-01 20:28:52 -0800

    first commit

---
+++ tests/basic_test.py
@@ -0,0 +1,12 @@
+import unittest
+
+
+class BasicTestSuite(unittest.TestCase):
+    """Basic test cases."""
+
+    def test_(self):
+        assert True
+
+
+if __name__ == '__main__':
+    unittest.main()

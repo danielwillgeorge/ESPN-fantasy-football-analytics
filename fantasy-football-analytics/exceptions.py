448076d4534a6c8ce063dd7a5579b262d8743c85
Daniel George <daniel.will.george@gmail.com>
2016-05-22 16:20:05 -0700

    first commit

---
+++ fantasy-football-analytics/exceptions.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+"""
+ESPN-analytics.exceptions
+~~~~~~~~~~~~
+
+This module contains the set of ESPN-analytics' exceptions.
+
+__author__ = "Daniel George"
+__license__ = MIT, see LICENSE.txt for more details.
+
+"""

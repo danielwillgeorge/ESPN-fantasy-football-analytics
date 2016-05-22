e0e1608f978f5f6b6ace61c934d5f65692049cae
Daniel George <daniel.will.george@gmail.com>
2016-05-22 16:19:48 -0700

    first commit

---
+++ fantasy-football-analytics/structures.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+"""
+ESPN-analytics.structures
+~~~~~~~~~~~~
+
+Data structures that power ESPN-analytics.
+
+__author__ = "Daniel George"
+__license__ = MIT, see LICENSE.txt for more details.
+
+"""

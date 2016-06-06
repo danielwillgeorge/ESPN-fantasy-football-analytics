b079a5f3d008554ac140a9a5a359e0124c37b577
Daniel George <daniel.will.george@gmail.com>
2016-06-06 15:21:51 -0700

    first commit

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+  name = 'espnfantasyfootball',
+  packages = ['espnfantasyfootball'], # this must be the same as the name above
+  version = '0.1',
+  description = 'A way to quickly retrieve ESPN team, game, and player data for a strategic edge in Fantasy Football leagues.',
+  author = 'Daniel Will George',
+  author_email = 'daniel.will.george@gmail.com',
+  url = 'https://github.com/danielwillgeorge/ESPN-fantasy-football-analytics', # use the URL to the github repo
+  #download_url = 'https://github.com/peterldowns/mypackage/tarball/0.1',
+  keywords = ['testing', 'logging', 'example'], # arbitrary keywords
+  classifiers = [],
+)

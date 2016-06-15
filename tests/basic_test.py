bcff9332e1009e63935df156092110cf96ac044c
Daniel George <daniel.will.george@gmail.com>
2016-06-15 15:32:16 -0700

    added basic test for teams method

--- tests/basic_test.py
+++ tests/basic_test.py
@@ -1,11 +1,14 @@
 import unittest
+from api import teams
+
+"""Tests for ESPN-fantasy-football-analytics."""
 
 
 class BasicTestSuite(unittest.TestCase):
     """Basic test cases."""
 
-    def test_(self):
-        assert True
+    def test_is_teams_success(self):
+        self.assertTrue(teams())
 
 
 if __name__ == '__main__':

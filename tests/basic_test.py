e595a6491bad1300e004ffc7a89a615f0ef3e78d
Daniel George <daniel.will.george@gmail.com>
2016-06-15 15:42:38 -0700

    test for isinstance Pandas DataFrame

--- tests/basic_test.py
+++ tests/basic_test.py
@@ -1,4 +1,9 @@
 import unittest
+import sys
+import pandas as pd
+
+sys.path.append("/Users/daniel.george/Desktop/github/ESPN-fantasy-football-analytics/fantasy-football-analytics")
+
 from api import teams
 
 """Tests for ESPN-fantasy-football-analytics."""
@@ -8,7 +13,7 @@ class BasicTestSuite(unittest.TestCase):
     """Basic test cases."""
 
     def test_is_teams_success(self):
-        self.assertTrue(teams())
+        self.assertIsInstance(teams(), pd.DataFrame)
 
 
 if __name__ == '__main__':

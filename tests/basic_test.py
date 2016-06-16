2ebe62217f646ede745f5e148902d8570dba09dd
Daniel George <daniel.will.george@gmail.com>
2016-06-16 16:27:12 -0700

    add tests for games and players methods

--- tests/basic_test.py
+++ tests/basic_test.py
@@ -2,11 +2,10 @@ import unittest
 import sys
 import pandas as pd
 
-sys.path.append("/Users/daniel.george/Desktop/github/ESPN-fantasy-football-analytics/fantasy-football-analytics")
-
 import espnfantasyfootball
 from espnfantasyfootball.api import teams
 
+#sys.path.append("/Users/daniel.george/Desktop/github/ESPN-fantasy-football-analytics/fantasy-football-analytics")
 #from api import teams
 
 """Tests for ESPN-fantasy-football-analytics."""
@@ -17,6 +16,12 @@ class BasicTestSuite(unittest.TestCase):
 
     def test_is_teams_success(self):
         self.assertIsInstance(teams(), pd.DataFrame)
+        
+    def test_is_games_success(self):
+        self.assertIsInstance(games(2015), pd.DataFrame)
+        
+    def test_is_players_success(self):
+        self.assertIsInstance(players(400554447), pd.DataFrame)
 
 
 if __name__ == '__main__':
